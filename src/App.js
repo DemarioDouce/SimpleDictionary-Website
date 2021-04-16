b64b511815e83f3c3db52d37596a1dfe874dfb78
DemarioDouce <demario.douce567@gmail.com>
2021-04-16 13:37:22 -0400

    UI improvements.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,40 @@ function App() {
       <div
         class="container-fluid"
         style={{
-          backgroundColor: "#fff328",
+          backgroundColor: "#b0efeb",
           textAlign: "center",
           padding: "6%",
         }}
       >
         <h1>SEARCH FOR A WORD</h1>
         <form>
-          <input type="text" class="form-control" id="search" />
-          <button type="submit" class="btn btn-primary">
-            Submit
+          <input
+            type="text"
+            class="form-control"
+            style={{
+              width: "50%",
+              padding: "12px 20px",
+              margin: "8px auto 0 auto",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+              boxSizing: "border-box",
+            }}
+            id="search"
+          />
+          <button
+            type="submit"
+            class="btn"
+            style={{
+              backgroundColor: "#edffa9",
+              color: "#000",
+              padding: "10px 30px",
+              fontSize: "16px",
+              margin: "10px 10px",
+              border: "2px solid #fff",
+              fontWeight: "bold",
+            }}
+          >
+            SEARCH
           </button>
         </form>
       </div>
