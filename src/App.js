7f86aa28d3863e92e74222284195fd7ba847c3e5
DemarioDouce <demario.douce567@gmail.com>
2021-04-16 15:15:40 -0400

    Changes and improvements

--- src/App.js
+++ src/App.js
@@ -9,50 +9,54 @@ function App() {
   return (
     <>
       {/* Main container */}
-      <div
-        class="container-fluid"
-        style={{
-          backgroundColor: "#b0efeb",
-          textAlign: "center",
-          padding: "6%",
-        }}
-      >
-        {/* Header text */}
-        <h1 style={{ fontWeight: "bold" }}>SEARCH FOR A WORD</h1>
-        {/* Form */}
-        <form>
-          {/* Input */}
-          <input
-            type="text"
-            class="form-control"
-            style={{
-              width: "50%",
-              padding: "12px 20px",
-              margin: "8px auto 0 auto",
-              border: "1px solid #ccc",
-              borderRadius: "4px",
-              boxSizing: "border-box",
-            }}
-            onChange={(e) => setSearchText(e.target.value)}
-            required
-          />
-          {/* Submit button */}
-          <button
-            type="submit"
-            class="btn"
-            style={{
-              backgroundColor: "#edffa9",
-              color: "#000",
-              padding: "10px 30px",
-              fontSize: "16px",
-              margin: "10px 10px",
-              border: "1px solid #fff",
-              fontWeight: "bold",
-            }}
-          >
-            SEARCH
-          </button>
-        </form>
+      <div class="container-fluid">
+        {/* Top */}
+        <div
+          style={{
+            backgroundColor: "#b0efeb",
+            textAlign: "center",
+            padding: "6%",
+          }}
+        >
+          {/* Top header text */}
+          <h1 style={{ fontWeight: "bold" }}>SEARCH FOR A WORD</h1>
+          {/* Form */}
+          <form>
+            {/* Input */}
+            <input
+              type="text"
+              class="form-control"
+              style={{
+                width: "50%",
+                padding: "12px 20px",
+                margin: "8px auto 0 auto",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                boxSizing: "border-box",
+              }}
+              onChange={(e) => setSearchText(e.target.value)}
+              required
+            />
+            {/* Submit button */}
+            <button
+              type="submit"
+              class="btn"
+              style={{
+                backgroundColor: "#edffa9",
+                color: "#000",
+                padding: "10px 30px",
+                fontSize: "16px",
+                margin: "10px 10px",
+                border: "1px solid #fff",
+                fontWeight: "bold",
+              }}
+            >
+              SEARCH
+            </button>
+          </form>
+        </div>
+        {/* Middle */}
+        <div>{searchText}</div>
       </div>
     </>
   );
