3d5fc333f28812855af7ff00ee1de2d142908f44
DemarioDouce <demario.douce567@gmail.com>
2021-04-16 12:21:52 -0400

    Init layout.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <div
+        class="container-fluid"
+        style={{
+          backgroundColor: "#fff328",
+          textAlign: "center",
+          padding: "6%",
+        }}
+      >
+        <h1>SEARCH FOR A WORD</h1>
+        <form>
+          <input type="text" class="form-control" id="search" />
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
